35f3f80dde046030cbc5a67d16dd6cdf381dcf43
Samuel Bleau <samuel.bleau@epitech.eu>
2026-02-08 10:32:44 +0100

    feat: enhance type checker with assignment handling, block statements, and function call validation

--- src/semantic/type_checker.rs
+++ src/semantic/type_checker.rs
@@ -8,6 +8,11 @@ pub enum TypeError {
     TypeMismatch { expected: Type, found: Type },
     InvalidOperand(String),
     ReturnTypeMismatch,
+    CannotAssignToFunction(String),
+    ArgumentCountMismatch { expected: usize, found: usize },
+    ArgumentTypeMismatch { position: usize, expected: Type, found: Type },
+    NotCallable,
+    InvalidCallTarget,
 }
 
 pub struct TypeChecker {
@@ -53,8 +58,11 @@ impl TypeChecker {
                 Ok(())
             }
 
-            Stmt::Return(expr) => {
-                let return_type = self.infer_expr(expr)?;
+            Stmt::Return(expr_opt) => {
+                let return_type = match expr_opt {
+                    Some(expr) => self.infer_expr(expr)?,
+                    None => Type::Void,
+                };
 
                 if let Some(expected) = &self.current_function_return_type {
                     if expected != &return_type {
@@ -91,6 +99,61 @@ impl TypeChecker {
                 Ok(())
             }
 
+            Stmt::Block(stmts) => {
+                self.symbol_table.enter_scope();
+                for stmt in stmts {
+                    self.check_stmt(stmt)?;
+                }
+                self.symbol_table.exit_scope();
+                Ok(())
+            }
+
+            Stmt::Expression(expr) => {
+                self.infer_expr(expr)?;
+                Ok(())
+            }
+
+            Stmt::Assignment { name, value } => {
+                let symbol = self.symbol_table.get(name)
+                    .ok_or_else(|| TypeError::UndefinedVariable(name.clone()))?;
+
+                let var_type = match symbol {
+                    Symbol::Variable(ty) => ty.clone(),
+                    Symbol::Function { .. } => {
+                        return Err(TypeError::CannotAssignToFunction(name.clone()));
+                    }
+                };
+
+                let value_type = self.infer_expr(value)?;
+
+                if var_type != value_type {
+                    return Err(TypeError::TypeMismatch {
+                        expected: var_type,
+                        found: value_type,
+                    });
+                }
+
+                Ok(())
+            }
+
+            Stmt::If { condition, then_branch, else_branch } => {
+                let cond_type = self.infer_expr(condition)?;
+                if cond_type != Type::Bool {
+                    return Err(TypeError::TypeMismatch {
+                        expected: Type::Bool,
+                        found: cond_type,
+                    });
+                }
+
+                self.check_stmt(then_branch)?;
+
+                if let Some(else_stmt) = else_branch {
+                    self.check_stmt(else_stmt)?;
+                }
+
+                Ok(())
+            }
+
             _ => Ok(()),
         }
     }
@@ -124,7 +187,39 @@ impl TypeChecker {
                 }
                 Ok(left_type)
             }
-            _ => Ok(Type::Int), //TODO: Handle other expression types and operators
+            Expr::Literal(Literal::Char(_)) => Ok(Type::Char),
+            Expr::Call { callee, args } => {
+                let func_symbol = if let Expr::Variable(name) = callee.as_ref() {
+                self.symbol_table.get(name)
+                    .ok_or_else(|| TypeError::UndefinedVariable(name.clone()))?
+                } else {
+                    return Err(TypeError::InvalidCallTarget);
+                };
+                match func_symbol {
+                    Symbol::Function { params, return_type } => {
+                        if params.len() != args.len() {
+                            return Err(TypeError::ArgumentCountMismatch {
+                                expected: params.len(),
+                                found: args.len(),
+                            });
+                        }
+
+                        for (i, (arg, expected_type)) in args.iter().zip(params.iter()).enumerate() {
+                            let arg_type = self.infer_expr(arg)?;
+                            if &arg_type != expected_type {
+                                return Err(TypeError::ArgumentTypeMismatch {
+                                    index: i,
+                                    expected: expected_type.clone(),
+                                    found: arg_type,
+                                });
+                            }
+                        }
+                        Ok(return_type.clone())
+                    }
+                    Symbol::Variable(_) => Err(TypeError::NotCallable)
+                }
+            }
+            // _ => Ok(Type::Int), TODO: Take this off
         }
     }
 }
